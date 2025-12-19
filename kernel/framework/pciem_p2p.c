0c820e6cf518a1fbdfa61ac284a76c0d0d522190
Carlos López <clopez@suse.de>
2025-12-19 19:47:32 +0100

    framework/p2p: store manager after successful initialization

--- kernel/framework/pciem_p2p.c
+++ kernel/framework/pciem_p2p.c
@@ -110,17 +110,15 @@ int pciem_p2p_init(struct pciem_root_complex *v, const char *regions_str)
     mgr->max_transfer_size = PCIEM_P2P_MAX_TRANSFER;
     mgr->enabled = false;
 
-    v->p2p_mgr = mgr;
-
     ret = parse_p2p_regions(mgr, regions_str);
     if (ret < 0) {
         pr_err("Failed to parse P2P regions: %d\n", ret);
         mutex_destroy(&mgr->lock);
         kfree(mgr);
-        v->p2p_mgr = NULL;
         return ret;
     }
 
+    v->p2p_mgr = mgr;
     return 0;
 }
 EXPORT_SYMBOL(pciem_p2p_init);
